05e8f594f966bb54b8b55ffa6a34065d13a92fb3
AngeTia <tiaange03@gmail.com>
2023-02-04 12:45:16 +0000

    Update content

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // Js detect the all error in code
 'use strict';
 
-const switcher = document.querySelector(".btn");
+const getElement = document.querySelector(".btn");
 
-switcher.addEventListener('click', () => {
+getElement.addEventListener('click', () => {
     // Toggle to modidfied body class
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     
     // Update button at theme
-    const className = document.body.className;
-    if (className == "light-theme") {
+    const getClassName = document.body.getClassName;
+    if (getClassName == "light-theme") {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
     }
     // Give the current class in console
-    console.log('current class name: ' + className);
+    console.log('current class name: ' + getClassName);
 });
+
+/** By Ange Tia */
